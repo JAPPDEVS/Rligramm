0232acf363fc94b4bc1c91b597ae8a0a2e76282f
JAPP <161047735+JAPPDEVS@users.noreply.github.com>
2024-02-23 18:47:47 +0400

    Initial commit

---
+++ app/src/main/java/com/example/rligramm/adapters/RecentConvarsationsAdapter.java
@@ -0,0 +1,77 @@
+package com.example.rligramm.adapters;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.rligramm.databinding.ItemContainerRecentConversationBinding;
+import com.example.rligramm.listeners.ConversationListener;
+import com.example.rligramm.models.ChatMessage;
+import com.example.rligramm.models.User;
+
+import java.util.List;
+
+public class RecentConvarsationsAdapter extends RecyclerView.Adapter<RecentConvarsationsAdapter.ConversationViewHolder> {
+    private final List<ChatMessage> chatMessages;
+    private final ConversationListener conversationListener;
+
+    public RecentConvarsationsAdapter(List<ChatMessage> chatMessages, ConversationListener conversationListener) {
+        this.chatMessages = chatMessages;
+        this.conversationListener = conversationListener;
+    }
+
+    @NonNull
+    @Override
+    public ConversationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ConversationViewHolder(
+                ItemContainerRecentConversationBinding.inflate(
+                        LayoutInflater.from(parent.getContext()),
+                        parent,
+                        false
+                )
+        );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ConversationViewHolder holder, int position) {
+        holder.setData(chatMessages.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return chatMessages.size();
+    }
+
+    class ConversationViewHolder extends RecyclerView.ViewHolder{
+        ItemContainerRecentConversationBinding binding;
+
+        public ConversationViewHolder(ItemContainerRecentConversationBinding Ibinding) {
+            super(Ibinding.getRoot());
+            binding = Ibinding;
+        }
+
+        void setData(ChatMessage chatMessage){
+            binding.imageProfile.setImageBitmap(getConversationImage(chatMessage.conversationImage));
+            binding.textName.setText(chatMessage.conversationName);
+            binding.textRecentMessage.setText(chatMessage.message);
+            binding.getRoot().setOnClickListener(v->{
+                User user = new User();
+                user.id = chatMessage.conversationId;
+                user.name = chatMessage.conversationName;
+                user.image = chatMessage.conversationImage;
+                conversationListener.onCoversationClicked(user);
+            });
+        }
+    }
+
+    private Bitmap getConversationImage(String encodedImage){
+        byte[] bytes = Base64.decode(encodedImage, Base64.DEFAULT);
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+    }
+}
---
+++ app/src/main/java/com/example/rligramm/listeners/ConversationListener.java
@@ -0,0 +1,7 @@
+package com.example.rligramm.listeners;
+
+import com.example.rligramm.models.User;
+
+public interface ConversationListener {
+    void onCoversationClicked(User user);
+}
